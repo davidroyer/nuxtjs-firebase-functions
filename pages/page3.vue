aaa4ebabbbced14d5659c8862d50d1c2a1f56b13
David Royer <droyer01@gmail.com>
2018-01-30 01:14:16 -0500

    Initial commit

---
+++ pages/page3.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        Page 3
+      </h1>
+      <h2 class="subtitle">
+        Some subtitle here
+      </h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia at doloribus nesciunt animi sed eum nemo neque dolor ad nihil libero adipisci perspiciatis, error impedit soluta eos quibusdam explicabo ullam culpa repellat quos provident, consequuntur excepturi. Debitis, nostrum, accusamus? Quos obcaecati libero atque ratione placeat praesentium nam odit nulla iusto.</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
