b4f1be7885077a2924da2479f65c39a3b67f45be
David Royer <droyer01@gmail.com>
2018-01-30 03:30:06 -0500

    app doesnt work

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,8 @@
       <h2 class="subtitle">
         Nuxt.js project
       </h2>
+      <h4 class="vertical-space">Rendered From:  <span>{{renderSource}}</span></h4>
+
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
@@ -20,6 +22,11 @@
 import Logo from '~/components/Logo.vue'
 
 export default {
+  asyncData () {
+    return {
+      renderSource: process.static ? 'static' : (process.server ? 'server' : 'client')
+    }
+  },
   components: {
     Logo
   }
--- pages/page3.vue
+++ pages/page3.vue
@@ -7,6 +7,7 @@
       <h2 class="subtitle">
         Some subtitle here
       </h2>
+      <h4 class="vertical-space">Rendered From:  <span>{{renderSource}}</span></h4>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia at doloribus nesciunt animi sed eum nemo neque dolor ad nihil libero adipisci perspiciatis, error impedit soluta eos quibusdam explicabo ullam culpa repellat quos provident, consequuntur excepturi. Debitis, nostrum, accusamus? Quos obcaecati libero atque ratione placeat praesentium nam odit nulla iusto.</p>
     </div>
   </section>
@@ -14,6 +15,11 @@
 
 <script>
 export default {
+  asyncData () {
+    return {
+      renderSource: process.static ? 'static' : (process.server ? 'server' : 'client')
+    }
+  }
 }
 </script>
 
